eaf6e8b3d9122b2ee213e17cb08396a3c9b264a2
jareyna-uci <97317915+jareyna-uci@users.noreply.github.com>
2023-05-01 20:35:26 -0700

    Init Report Class
    The report class will keep track of all the data that will be needed for the final report

--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@ import urllib.robotparser as urllib_rp
 from bs4 import BeautifulSoup
 from textProcessor import TextProcessor
 from hashlib import sha256
+from collections import defaultdict
 
 class TextSimilarityProcessor:  #class for hasing (getting the fingerprint) of the webpage text and
                                 #comparing it against the last crawled webpage to detect traps
@@ -71,6 +72,62 @@ class Robots:   #The Robots class checks if the url is allowed in their respecti
             return Robots.stat.can_fetch('*', url)
         if re.match(r".*\.informatics\.uci\.edu.*", netloc):
             return Robots.informatics.can_fetch('*', url)
+        
+class Report:
+    unique_pages = 0
+    longest_page = 0
+    words_freq = defaultdict(int)
+    sub_domains = set()
+    stop_words = ["a", "able", "about", "above", "abst", "accordance", "according", "accordingly", "across", "act", 
+        "actually", "added", "adj", "affected", "affecting", "affects", "after", "afterwards", "again", "against", 
+        "ah", "all", "almost", "alone", "along", "already", "also", "although", "always", "am", 
+        "among", "amongst", "an", "and", "announce", "another", "any", "anybody", "anyhow", "anymore", 
+        "anyone", "anything", "anyway", "anyways", "anywhere", "apparently", "approximately", "are", "aren", "arent", 
+        "arise", "around", "as", "aside", "ask", "asking", "at", "auth", "available", "away", 
+        "awfully", "b", "back", "be", "became", "because", "become", "becomes", "becoming", "been", 
+        "before", "beforehand", "begin", "beginning", "beginnings", "begins", "behind", "being", "believe", "below", 
+        "beside", "besides", "between", "beyond", "biol", "both", "brief", "briefly", "but", "by", 
+        "c", "ca", "came", "can", "cannot", "can't", "cause", "causes", "certain", "certainly", 
+        "co", "com", "come", "comes", "contain", "containing", "contains", "could", "couldnt", "d", 
+        "date", "did", "didn't", "different", "do", "does", "doesn't", "doing", "done", "don't", 
+        "down", "downwards", "due", "during", "e", "each", "ed", "edu", "effect", "eg", 
+        "eight", "eighty", "either", "else", "elsewhere", "end", "ending", "enough", "especially", "et", 
+        "et-al", "etc", "even", "ever", "every", "everybody", "everyone", "everything", "everywhere", "ex", 
+        "except", "f", "far", "few", "ff", "fifth", "first", "five", "fix", "followed", 
+        "following", "follows", "for", "former", "formerly", "forth", "found", "four", "from", "further", 
+        "furthermore", "g", "gave", "get", "gets", "getting", "give", "given", "gives", "giving", 
+        "go", "goes", "gone", "got", "gotten", "h", "had", "happens", "hardly", "has", 
+        "hasn't", "have", "haven't", "having", "he", "hed", "hence", "her", "here", "hereafter", 
+        "hereby", "herein", "heres", "hereupon", "hers", "herself", "hes", "hi", "hid", "him", 
+        "himself", "his", "hither", "home", "how", "howbeit", "however", "hundred", "i", "id", 
+        "ie", "if", "i'll", "im", "immediate", "immediately", "importance", "important", "in", "inc", 
+        "indeed", "index", "information", "instead", "into", "invention", "inward", "is", "isn't", "it", 
+        "itd", "it'll", "its", "itself", "i've", "j", "just", "k", "keep	keeps", "kept", 
+        "kg", "km", "know", "known", "knows", "l", "largely", "last", "lately", "later", 
+        "latter", "latterly", "least", "less", "lest", "let", "lets", "like", "liked", "likely", 
+        "line", "little", "'ll", "look", "looking", "looks", "ltd", "m", "made", "mainly", 
+        "make", "makes", "many", "may", "maybe", "me", "mean", "means", "meantime", "meanwhile", 
+        "merely", "mg", "might", "million", "miss", "ml", "more", "moreover", "most", "mostly", 
+        "mr", "mrs", "much", "mug", "must", "my", "myself", "n", "na", "name", 
+        "namely", "nay", "nd", "near", "nearly", "necessarily", "necessary", "need", "needs", "neither", 
+        "never", "nevertheless", "new", "next", "nine", "ninety", "no", "nobody", "non", "none", 
+        "nonetheless", "noone", "nor", "normally", "nos", "not", "noted", "nothing", "now", "nowhere", 
+        "o", "obtain", "obtained", "obviously", "of", "off", "often", "oh", "ok", "okay", 
+        "old", "omitted", "on", "once", "one", "ones", "only", "onto", "or", "ord", 
+        "other", "others", "otherwise", "ought", "our", "ours", "ourselves", "out", "outside", "over", 
+        "overall", "owing", "own", "p", "page", "pages", "part", "particular", "particularly", "past", 
+        "per", "perhaps", "placed", "please", "plus", "poorly", "possible", "possibly", "potentially", "pp", 
+        "predominantly", "present", "previously", "primarily", "probably", "promptly", "proud", "provides", "put", "q", 
+        "que", "quickly", "quite", "qv", "r", "ran", "rather", "rd", "re", "readily", 
+        "really", "recent", "recently", "ref", "refs", "regarding", "regardless", "regards", "related", "relatively", 
+        "research", "respectively", "resulted", "resulting", "results", "right", "run", "s", "said", "same", 
+        "saw", "say", "saying", "says", "sec", "section", "see", "seeing", "seem", "seemed", 
+        "seeming", "seems", "seen", "self", "selves", "sent", "seven", "several", "shall", "she", 
+        "shed", "she'll", "shes", "should", "shouldn't", "show", "showed", "shown", "showns", "shows", 
+        "significant", "significantly", "similar", "similarly", "since", "six", "slightly", "so", "some", "somebody", 
+        "somehow", "someone", "somethan", "something", "sometime", "sometimes", "somewhat", "somewhere", "soon", "sorry", 
+        "specifically", "specified", "specify", "specifying", "still", "stop", "strongly", "sub", "substantially", "successfully", 
+        "such", "sufficiently", "suggest", "sup", "sure"]
 
 
 def scraper(url, resp):
