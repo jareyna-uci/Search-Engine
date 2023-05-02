ba55e64991bbf52199de9f3ce6b9aa865a434359
jareyna-uci <97317915+jareyna-uci@users.noreply.github.com>
2023-05-01 23:37:00 -0700

    Stop Words In Tokenizer
    Moved the stop words to the Text Processor class instead so we can work with relevant content when working with tokens from page content.

--- scraper.py
+++ scraper.py
@@ -6,13 +6,15 @@ from textProcessor import TextProcessor
 from hashlib import sha256
 from collections import defaultdict
 
+TP = TextProcessor()
+
 class TextSimilarityProcessor:  #class for hasing (getting the fingerprint) of the webpage text and
                                 #comparing it against the last crawled webpage to detect traps
     last_url_hash = ""  #This class compares against the last file downloaded
 
     def get_fingerprint(resp):  #gets the 32-bit fingerprint
         text = resp.get_text(strip=True)    #this function only uses the test of the html file to generate the fingerprint
-        text_freq = TextProcessor.computeWordFrequencies(TextProcessor.tokenize(text))
+        text_freq = TP.computeWordFrequencies(TP.tokenize())
         vector = [0] * 32
 
         for token, weight in text_freq.items():
@@ -80,56 +82,6 @@ class Report:
     sub_domains = set() # set of sub domains seen so far
     seen_urls = set()   # set of urls seen
     N = 0   # top N words frequencies that would be presented
-    stop_words = ["a", "able", "about", "above", "abst", "accordance", "according", "accordingly", "across", "act", 
-        "actually", "added", "adj", "affected", "affecting", "affects", "after", "afterwards", "again", "against", 
-        "ah", "all", "almost", "alone", "along", "already", "also", "although", "always", "am", 
-        "among", "amongst", "an", "and", "announce", "another", "any", "anybody", "anyhow", "anymore", 
-        "anyone", "anything", "anyway", "anyways", "anywhere", "apparently", "approximately", "are", "aren", "arent", 
-        "arise", "around", "as", "aside", "ask", "asking", "at", "auth", "available", "away", 
-        "awfully", "b", "back", "be", "became", "because", "become", "becomes", "becoming", "been", 
-        "before", "beforehand", "begin", "beginning", "beginnings", "begins", "behind", "being", "believe", "below", 
-        "beside", "besides", "between", "beyond", "biol", "both", "brief", "briefly", "but", "by", 
-        "c", "ca", "came", "can", "cannot", "can't", "cause", "causes", "certain", "certainly", 
-        "co", "com", "come", "comes", "contain", "containing", "contains", "could", "couldnt", "d", 
-        "date", "did", "didn't", "different", "do", "does", "doesn't", "doing", "done", "don't", 
-        "down", "downwards", "due", "during", "e", "each", "ed", "edu", "effect", "eg", 
-        "eight", "eighty", "either", "else", "elsewhere", "end", "ending", "enough", "especially", "et", 
-        "et-al", "etc", "even", "ever", "every", "everybody", "everyone", "everything", "everywhere", "ex", 
-        "except", "f", "far", "few", "ff", "fifth", "first", "five", "fix", "followed", 
-        "following", "follows", "for", "former", "formerly", "forth", "found", "four", "from", "further", 
-        "furthermore", "g", "gave", "get", "gets", "getting", "give", "given", "gives", "giving", 
-        "go", "goes", "gone", "got", "gotten", "h", "had", "happens", "hardly", "has", 
-        "hasn't", "have", "haven't", "having", "he", "hed", "hence", "her", "here", "hereafter", 
-        "hereby", "herein", "heres", "hereupon", "hers", "herself", "hes", "hi", "hid", "him", 
-        "himself", "his", "hither", "home", "how", "howbeit", "however", "hundred", "i", "id", 
-        "ie", "if", "i'll", "im", "immediate", "immediately", "importance", "important", "in", "inc", 
-        "indeed", "index", "information", "instead", "into", "invention", "inward", "is", "isn't", "it", 
-        "itd", "it'll", "its", "itself", "i've", "j", "just", "k", "keep	keeps", "kept", 
-        "kg", "km", "know", "known", "knows", "l", "largely", "last", "lately", "later", 
-        "latter", "latterly", "least", "less", "lest", "let", "lets", "like", "liked", "likely", 
-        "line", "little", "'ll", "look", "looking", "looks", "ltd", "m", "made", "mainly", 
-        "make", "makes", "many", "may", "maybe", "me", "mean", "means", "meantime", "meanwhile", 
-        "merely", "mg", "might", "million", "miss", "ml", "more", "moreover", "most", "mostly", 
-        "mr", "mrs", "much", "mug", "must", "my", "myself", "n", "na", "name", 
-        "namely", "nay", "nd", "near", "nearly", "necessarily", "necessary", "need", "needs", "neither", 
-        "never", "nevertheless", "new", "next", "nine", "ninety", "no", "nobody", "non", "none", 
-        "nonetheless", "noone", "nor", "normally", "nos", "not", "noted", "nothing", "now", "nowhere", 
-        "o", "obtain", "obtained", "obviously", "of", "off", "often", "oh", "ok", "okay", 
-        "old", "omitted", "on", "once", "one", "ones", "only", "onto", "or", "ord", 
-        "other", "others", "otherwise", "ought", "our", "ours", "ourselves", "out", "outside", "over", 
-        "overall", "owing", "own", "p", "page", "pages", "part", "particular", "particularly", "past", 
-        "per", "perhaps", "placed", "please", "plus", "poorly", "possible", "possibly", "potentially", "pp", 
-        "predominantly", "present", "previously", "primarily", "probably", "promptly", "proud", "provides", "put", "q", 
-        "que", "quickly", "quite", "qv", "r", "ran", "rather", "rd", "re", "readily", 
-        "really", "recent", "recently", "ref", "refs", "regarding", "regardless", "regards", "related", "relatively", 
-        "research", "respectively", "resulted", "resulting", "results", "right", "run", "s", "said", "same", 
-        "saw", "say", "saying", "says", "sec", "section", "see", "seeing", "seem", "seemed", 
-        "seeming", "seems", "seen", "self", "selves", "sent", "seven", "several", "shall", "she", 
-        "shed", "she'll", "shes", "should", "shouldn't", "show", "showed", "shown", "showns", "shows", 
-        "significant", "significantly", "similar", "similarly", "since", "six", "slightly", "so", "some", "somebody", 
-        "somehow", "someone", "somethan", "something", "sometime", "sometimes", "somewhat", "somewhere", "soon", "sorry", 
-        "specifically", "specified", "specify", "specifying", "still", "stop", "strongly", "sub", "substantially", "successfully", 
-        "such", "sufficiently", "suggest", "sup", "sure"]   # stop words
     
     def __init__(self, N) -> None:
         self.N = N  # initilizes the amount of top words frequencies
@@ -156,6 +108,7 @@ class Report:
     def update_word_freq(self, wordFreq):
         for k, v in wordFreq.items():
             self.word_freq[k] += v  # update word freq of words weve seen so far with new words seen
+        
 
 
 
